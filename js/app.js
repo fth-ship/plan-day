e3638274097113d09e2c5183fefeccb55213abb4
Kaique da Silva <kaique.developer@gmail.com>
2014-10-29 18:42:42 -0200

    Fixed new itens in scope Added range slider to support diff categories of time

--- js/app.js
+++ js/app.js
@@ -1,16 +1,22 @@
-var plan = getLocalPlanHandler() || [];
+var plan = getLocalPlanHandler();
 
 function setLocalPlanHandler(plan) {
   localStorage.setItem('plan', JSON.stringify(plan));
 }
 
 function getLocalPlanHandler() {
-  return JSON.parse(localStorage.getItem('plan')) || (plan || []);
+  return JSON.parse(localStorage.getItem('plan') || '[]');
 }
 
-function TimeInputCtrl($scope, $log, setLocalPlan, getLocalPlan) {
-  $scope.min = 0;
-  $scope.max = 24 / .5;
+function timeHandler(input) {
+  var timeRange = [];
+
+  return input;
+}
+
+function TimeInputCtrlHandler($scope, $log, setLocalPlan, getLocalPlan, $rootScope, min, max) {
+  $scope.min = min;
+  $scope.max = max;
   $scope.model = { 
     from: $scope.min,
     to: $scope.max,
@@ -20,26 +26,45 @@ function TimeInputCtrl($scope, $log, setLocalPlan, getLocalPlan) {
     $log.debug('save time handler');
     plan.push(model);
     setLocalPlan(plan);
-    TimeListCtrl.plan = getLocalPlan();
-    $scope.model = {};
+    plan = getLocalPlan();
+    $rootScope.$broadcast('plan', plan);
+    $scope.model = { 
+      from: $scope.min,
+      to: $scope.max,
+      shortDescription: ''
+    };
   }
   this.saveTime = saveTimeHandler;
 }
 
-function TimeListCtrl($scope, $log, getLocalPlan) {
-  $scope.plan = getLocalPlan();
+function TimeListCtrlHandler($scope, $log, getLocalPlan) {
+  $scope.plan = plan;
+
+  function planEventHandler(e, data) {
+    $scope.plan = data; 
+  }
+  $scope.$on('plan', planEventHandler);
 }
 
 angular
   .module('plan-day', ['ui-rangeSlider'])
+  .value('min', 0)
+  .value('max', (24 / .5))
   .factory('setLocalPlan', function () {
     return setLocalPlanHandler;
   })
   .factory('getLocalPlan', function () {
     return getLocalPlanHandler;
   })
-  .controller('TimeInputCtrl', TimeInputCtrl)
-  .controller('TimeListCtrl', TimeListCtrl)
+  .filter('time', function ($log, min, max) {
+    return timeHandler.bind({
+      $log: $log,
+      min: min,
+      max: max
+    });
+  })
+  .controller('TimeInputCtrl', TimeInputCtrlHandler)
+  .controller('TimeListCtrl', TimeListCtrlHandler)
   .run(function ($log) {
     $log.debug('Plan day running!');
   });
